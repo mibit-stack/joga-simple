c1595ec94cea8fa444b553ad031b450a6d72e669
Paulina Mrozik <paulajna.m@gmail.com>
2021-01-14 14:58:09 +0100

    Add snackbar to About layout

--- app/src/main/java/com/zpi/jogasimple/aboutActivity.kt
+++ app/src/main/java/com/zpi/jogasimple/aboutActivity.kt
@@ -1,11 +1,42 @@
 package com.zpi.jogasimple
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.mainView
+
 
 class aboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        /*if(item.itemId==android.R.id.home)
+            finish()
+        */
+
+        when(item.itemId){
+            android.R.id.home->finish()
+            R.id.id_info->{
+                return true
+            }
+            R.id.id_sound->{
+                Snackbar.make(mainView, "Kliknięto menu stop...", Snackbar.LENGTH_SHORT).show()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

3a47a3fecb7481d14825f03dfd7d3a886381b925
Paulina Mrozik <paulajna.m@gmail.com>
2021-02-12 09:14:06 +0100

    Bux fixes

--- app/src/main/java/com/zpi/jogasimple/ExcerciseActivity.kt
+++ app/src/main/java/com/zpi/jogasimple/ExcerciseActivity.kt
@@ -38,51 +38,51 @@ class ExcerciseActivity : AppCompatActivity() {
     val exerciseMorning = arrayOf(
         Exercise(R.drawable.ex_m_1, "Muktasana jest idealna pozą do chwili odpoczynku i medytacji. Ćwiczenie to ma na celu stopniowe rozbudzenie naszego organizmu po śnie i przygotowanie go do ruchu w następnych ćwiczeniach.\n" +
                 "\n" +
-                "W czasie trzech minut tego ćwiczenia skup się na głębokim i spokojnym oddechu.  Pamiętaj również o wyprostowanych plecach.\n",10),
+                "W czasie trzech minut tego ćwiczenia skup się na głębokim i spokojnym oddechu.  Pamiętaj również o wyprostowanych plecach.\n",45),
         Exercise(R.drawable.ex_m_2, "Z pozycji przyjętego wcześniej siadu wykonuj rekoma naprzemiennie skłony w lewy i prawy bok. Nie spiesz się, dokładność rozciągnięcia jest ważniejsza. \n" +
                 "\n" +
-                "Ćwiczenie to pomoże nam rozciągnąć ramiona i ręce, które często cierpną w nocy przez spanie w nieodpowiednich pozycjach.",5),
+                "Ćwiczenie to pomoże nam rozciągnąć ramiona i ręce, które często cierpną w nocy przez spanie w nieodpowiednich pozycjach.",45),
         Exercise(R.drawable.ex_m_3,"Setu bandhasana, inaczej pozycja mostka. \n" +
                 "Łopatki oraz góra pleców leżą na ziemi, podczas gdy biodra i zgięte w kolanach nogi podnoszą się ku górze.\n" +
                 "Ćwiczenie należy wykonywać powtórzeniami, uwzględniając przerwy pomiędzy nimi. W ramach przerw należy poleżeć przez parę sekund.\n" +
                 "\n" +
-                "Asana ta działa pozytywnie na plecy, miednicę, nogi i klatkę piersiowa. Przygotowuje nas idealnie na czekający dzień - pomaga usunąć napięcie z barków, uśmierzyć ból pleców.",5),
+                "Asana ta działa pozytywnie na plecy, miednicę, nogi i klatkę piersiowa. Przygotowuje nas idealnie na czekający dzień - pomaga usunąć napięcie z barków, uśmierzyć ból pleców.",45),
         Exercise(R.drawable.ex_m_4,"Klęknięcie na jedną nogę z wyrokiem w przód (Anjaneyasana)\n" +
                 "\n" +
                 "W ćwiczeniu należy pozostać dłuższą chwilę, po czym zmieniać kolejność na drugą nogę.\n" +
                 "\n" +
                 "Pamiętaj o prostych plecach i spokojnym oddechu.\n" +
-                "Ćwiczenie wspiera prawidłowe funkcjonowanie kolan.",5),
+                "Ćwiczenie wspiera prawidłowe funkcjonowanie kolan.",45),
         Exercise(R.drawable.ex_m_5,"Asana psa z głową w dół (Adho Mukha Svanasana).\n" +
                 "Osoby początkujące mogą stać na czubkach palców, ale w miarę postępów należy dążyć do postawienia stóp całkowicie na ziemi.\n" +
                 "\n" +
                 "Jest to jedna z najbardziej znanych póz jogi - używa się jej często jako relaksacyjnego przerywnika między sekwencjami ćwiczeń.\n" +
                 "\n" +
-                "Wpływa pozytywnie na krążenie krwi, wzmacnia ramiona i poprawia elastyczność stawów.",5))
+                "Wpływa pozytywnie na krążenie krwi, wzmacnia ramiona i poprawia elastyczność stawów.",45))
 
     val exerciseNight = arrayOf(
         Exercise(R.drawable.ex_n_1, "Adho mukhna virasana - skłon do przodu. \n" +
                 "Pozycja ta jest jedna z lepszych jeśli chodzi o odprężenie ciała i regenerację po ciężkim dniu.\n" +
                 "\n" +
-                "Ćwiczenie pomaga uelastycznić kręgosłup, rozluźnia mięśnie szyi i intensywnie rozciąga kolana",10),
+                "Ćwiczenie pomaga uelastycznić kręgosłup, rozluźnia mięśnie szyi i intensywnie rozciąga kolana",45),
         Exercise(R.drawable.ex_n_2, " Siad klęczny z odchyleniem w tył. \n" +
                 "Należy przyjąć tą samą pozycję przez cały czas trwania ćwiczenia.\n" +
                 "\n" +
-                "Ćwiczenie pomaga rozluźnić mięśnie karku, ramion i wzmacnia zasięg odchylania pleców",2),
+                "Ćwiczenie pomaga rozluźnić mięśnie karku, ramion i wzmacnia zasięg odchylania pleców",45),
         Exercise(R.drawable.ex_n_3,"Siad klęczny z wyciągnięciem do przodu.\n" +
                 "Ćwiczenie należy wykonywać powtórzeniami z krótkimi przerwami pomiędzy.\n" +
                 "\n" +
                 "Pamiętaj żeby podczas ćwiczenia próbować sięgnąć jak najdalej.\n" +
                 "\n" +
-                "Ćwiczenie wspomaga ramiona, rozluźnia plecy i mięśnie karku.",2),
+                "Ćwiczenie wspomaga ramiona, rozluźnia plecy i mięśnie karku.",45),
         Exercise(R.drawable.ex_n_4,"Pozycja kota (Marjarysana).\n" +
                 "Wygięcia grzbietu należy utrzymywać przez dłuższą chwilę i uwzględniać chwile rozluźnienia między powtórzeniami.\n" +
                 "\n" +
-                "Ćwiczenie rewelacyjnie działa na kręgosłup wzmacniając go i rozluźniając przy tym plecy.",2),
+                "Ćwiczenie rewelacyjnie działa na kręgosłup wzmacniając go i rozluźniając przy tym plecy.",45),
         Exercise(R.drawable.ex_n_5,"Skręt w Svastikasanie. Ćwiczenie należy wykonywać naprzemiennie sięgnięciami za siebie. \n" +
                 "\n" +
                 "Należy pamiętać o prostych plecach i zsynchronizowaniu oddychania z wykonywanymi skrętami.\n" +
-                "Ćwiczenie wpływa pozytywnie na mięśnie ramion i ich rozciągnięcie.",2))
+                "Ćwiczenie wpływa pozytywnie na mięśnie ramion i ich rozciągnięcie.",45))
 
     var currentExcerciseSet = exerciseMorning
 
@@ -123,6 +123,7 @@ class ExcerciseActivity : AppCompatActivity() {
         exerciseDescription  = exercise.exDesc
 
         var underNineTxt = ""
+
         if (exerciseTime<10) {underNineTxt= "0"}
         timerView.setText("00:${underNineTxt}${exerciseTime}")
 
@@ -145,8 +146,7 @@ class ExcerciseActivity : AppCompatActivity() {
             }
 
             runOnUiThread{
-//                val toneG = ToneGenerator(AudioManager.STREAM_ALARM, 100)
-//                toneG.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT,500)
+
                 var mediaPlayer = MediaPlayer.create(this, R.raw.zapsplat_bells_medium_bell_soft_strike_long_decay_002_60153)
                 mediaPlayer.start()
 
